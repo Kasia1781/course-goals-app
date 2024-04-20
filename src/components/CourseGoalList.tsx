bae56cbbdb67784e7b353a3c5e59950f151ffcd9
Kasia <kasia1781@gmail.com>
2024-04-20 19:09:44 +0200

    created InfoBox component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,4 +1,5 @@
 import CourseGoal from './CourseGoal';
+import InfoBox from './InfoBox';
 
 type CourseGoalListProps = {
 	goals: {
@@ -13,15 +14,34 @@ export default function CourseGoalList({
 	goals,
 	onDeleteGoal,
 }: CourseGoalListProps) {
+	if (goals.length === 0) {
+		return (
+			<InfoBox mode='hint'>
+				You haven't got any goals yet. Start adding some!
+			</InfoBox>
+		);
+	}
+
+	let warningBox: ReactNode;
+
+	if (goals.length >= 4) {
+		warningBox = (
+			<InfoBox mode='warning' severity='high'>Masz zbyt wiele celów na liście</InfoBox>
+		);
+	}
+
 	return (
-		<ul>
-			{goals.map((goal) => (
-				<li key={goal.id}>
-					<CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-						{goal.description}
-					</CourseGoal>
-				</li>
-			))}
-		</ul>
+		<>
+			{warningBox}
+			<ul>
+				{goals.map((goal) => (
+					<li key={goal.id}>
+						<CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+							{goal.description}
+						</CourseGoal>
+					</li>
+				))}
+			</ul>
+		</>
 	);
 }
